e43cdd83ab155676faf3dc6dd6d502b442d49f00
Oxana Lezhneva <oxan.kin@ya.ru>
2022-11-22 13:23:08 +0300

    Update HW_5.py

--- HW_5/HW_5.py
+++ HW_5/HW_5.py
@@ -21,6 +21,7 @@ def purchases():
 # содержимое purchase_log.txt помещается в оперативную память компьютера
 # содержимое visit_log.csv - нет; используйте только построчную обработку этого файла
 
+# первый вариант
 def visit_log():
 
     with open('visit_log.csv', "r",  encoding="utf-8") as f: 
@@ -32,6 +33,18 @@ def visit_log():
                 if i[0] in dict_purche:  
                     i.append(dict_purche[i[0]])                    
                     datawriter.writerow(i)
+                    
+# второй вариант                    
+def visit_log():
+
+    with open('visit_log.csv', "r",  encoding="utf-8") as f: 
+        with open('funnel.csv', "w",  encoding="utf-8",  newline='') as f2: 
+            dict_purche = purchases()
+            for item in f:
+                user_id = item.split(",")[0]
+                if user_id in  dict_purche:                
+                    join_word = f'{item.strip()},{dict_purche[user_id]}\n'
+                    f2.write( join_word)                    
                          
 visit_log()
 
