c709291c359f74d726c2abfbaf82af042035004f
Oxana Lezhneva <oxan.kin@ya.ru>
2022-11-24 10:57:27 +0300

    HW_6

---
+++ HW_6/HW_6.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from datetime import timedelta
+
+# Задание 1
+# Печатные газеты использовали свой формат дат для каждого выпуска. 
+# Для каждой газеты из списка напишите формат указанной даты для перевода в объект datetime:
+dict_date = {'The Moscow Times' : 'Wednesday, October 2, 2002', 'The Guardian' : 'Friday, 11.10.13', 'Daily News' : 'Thursday, 18 August 1977'}
+
+
+def str_datetime():
+
+    for i in dict_date:       
+        if i == 'The Moscow Times':
+            data = datetime.strptime(dict_date[i], '%A, %B %d, %Y')
+            print(data)
+        elif i == 'The Guardian':
+            data = datetime.strptime(dict_date[i], '%A, %d.%m.%y')
+            print(data)
+        elif i == 'Daily News':
+            data = datetime.strptime(dict_date[i], '%A, %d %B %Y')
+            print(data)
+
+
+# str_datetime()
+
+# Задание 2
+# Дан поток дат в формате YYYY-MM-DD, в которых встречаются некорректные значения:
+stream = ['2018-04-02', '2018-02-29', '2018-19-02']
+
+# Напишите функцию, которая проверяет эти даты на корректность. Т. е. 
+# для каждой даты возвращает True (дата корректна) или False (некорректная дата).
+
+
+def date_correctness():
+
+    month = ['01','02','03','04','05','06','07','08','09','10','11','12']
+    for item in stream:
+        print(item[5:7] in month)
+
+
+
+# date_correctness()
+
+# Задание 3
+# Напишите функцию date_range, которая возвращает список дат за период от start_date до end_date.
+# Даты должны вводиться в формате YYYY-MM-DD. 
+# В случае неверного формата или при start_date > end_date должен возвращаться пустой список.
+
+
+def date_range():
+
+    format_date = '%Y-%m-%d'
+    try: 
+        start_date = datetime.strptime((input("Введите дату начала: ").strip()), format_date)
+                 
+        end_date = datetime.strptime((input("Введите дату окончания: ").strip()), format_date)
+        range_days = int((end_date - start_date).days)       
+    
+        date_range = [(start_date + timedelta(days=x)).strftime(format_date) for x in range(range_days+1)]
+        print(date_range)
+    except:
+        print([])
+   
+# date_range()
+
