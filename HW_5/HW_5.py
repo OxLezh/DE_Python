eb3d577e7d1f49dc8d7b7cdc280ea677911aa0cf
Oxana Lezhneva <oxan.kin@ya.ru>
2022-11-24 11:01:53 +0300

    1

--- HW_5/HW_5.py
+++ HW_5/HW_5.py
@@ -2,6 +2,7 @@ import json
 import csv
 
 
+
 #  Переведите содержимое файла purchase_log.txt в словарь purchases вида:
 #  {‘1840e0b9d4’: ‘Продукты’, …}
 
@@ -9,8 +10,8 @@ def purchases():
     purchases ={}
     with open('purchase_log.txt', "r",  encoding="utf-8") as f:
         for line in f:       
-            key, value = (json.loads(line).items())
-            purchases[key[1]] = value[1]
+            line = json.loads(line)
+            purchases[line['user_id']] = line['category']
     return purchases
 
         
@@ -21,18 +22,34 @@ def purchases():
 # содержимое purchase_log.txt помещается в оперативную память компьютера
 # содержимое visit_log.csv - нет; используйте только построчную обработку этого файла
 
+# def visit_log():
+
+#     with open('visit_log.csv', "r",  encoding="utf-8") as f: 
+#         rows = list(csv.reader(f, delimiter=","))
+#         with open('funnel.csv', "w",  encoding="utf-8",  newline='') as f2:            
+#             datawriter = csv.writer(f2, delimiter=',')
+#             dict_purche = purchases()
+#             for i in rows:                    
+#                 if i[0] in dict_purche:
+#                     print( i[0])  
+                    # i.append(dict_purche[i[0]])                    
+                    # datawriter.writerow(i)
+                         
+# visit_log()
+
 def visit_log():
 
     with open('visit_log.csv', "r",  encoding="utf-8") as f: 
-        rows = list(csv.reader(f, delimiter=","))
-        with open('funnel.csv', "w",  encoding="utf-8",  newline='') as f2:            
-            datawriter = csv.writer(f2, delimiter=',')
+        with open('funnel.csv', "w",  encoding="utf-8",  newline='') as f2: 
             dict_purche = purchases()
-            for i in rows:                    
-                if i[0] in dict_purche:  
-                    i.append(dict_purche[i[0]])                    
-                    datawriter.writerow(i)
-                         
+            for item in f:
+                user_id = item.split(",")[0]
+                if user_id in  dict_purche:                
+                    b = f'{item.strip()},{dict_purche[user_id]}\n'
+                    f2.write(b)
+                
+                
+                
+  
+                        
 visit_log()
-
-   
